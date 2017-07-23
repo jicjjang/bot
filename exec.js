108d33b83aa05b818b4880142d2bec4bb3a0dbfa
jicjjang <jicjjang12@gmail.com>
2017-07-23 23:59:55 +0900

    append initailize load

--- exec.js
+++ exec.js
@@ -1,90 +1,180 @@
-var cssId = 'weeklyCss';
-var body = document.querySelector("body");
+var html = $('html');
+var htmlOverflowY = null;
+var body = $('body');
+var dimmed_weekly = $('#dimmed_weekly');
 
-if (!document.getElementById(cssId)) {
-    var head  = document.getElementsByTagName('head')[0];
-    var style  = document.createElement('style');
-    style.id   = cssId;
-    style.type = 'text/css';
+var initWeekly = function () {
+  $("<style type='text/css'>\
+    html.weekly {\
+      overflow-y: hidden;\
+    }\
+    #dimmed_weekly {\
+      width: 100%;\
+      position: fixed;\
+      height: 100%;\
+      overflow: auto;\
+      top: 0;\
+      left: 0;\
+      background-color: rgba(0,0,0,0.8);\
+      z-index: 99999;\
+    }\
+    #container_weekly {\
+      position: absolute;\
+      right: 0;\
+      top: 0;\
+      width: 440px;\
+      padding: 30px;\
+      background: white;\
+    }\
+    .infomation_weekly {\
+      position: fixed;\
+      width: 200px;\
+    }\
+    .infomation_weekly > h1 {\
+      margin: 0 0 10px 0;\
+    }\
+    .infomation_weekly > article > div > ul {\
+      margin: 0;\
+      list-style-type: none;\
+    }\
+    .infomation_weekly > article > div > ul > li {\
+      word-break: break-all;\
+    }\
+    .form_weekly {\
+      float: right;\
+    }\
+    .form_weekly::after {\
+      content: '';\
+      display: table;\
+      clear: both;\
+    }\
+    .form_weekly > article > .time {\
+      margin: 0 0 5px 0;\
+    }\
+    .form_weekly > article > .time > input {\
+    }\
+    .form_weekly > article > textarea {\
+      width: 200px;\
+      margin: 0 0 10px 0;\
+    }\
+  </style>").appendTo('head');
 
-    style.appendChild(document.createTextNode('#container_weekly {' +
-      'position: fixed;' +
-      'right: 0;' +
-      'top: 0;' +
-      'background: white;' +
-      'display: flex;' +
-      '-webkit-display: flex;' +
-      'width: 500px;' +
-    '}' +
+  body.append('\
+    <div id="dimmed_weekly">\
+      <div id="container_weekly">\
+        <section class="infomation_weekly">\
+          <h1>Weekly</h1>\
+          <article>\
+            <div>\
+              <h3>월요일</h3>\
+              <ul>\
+                <li>test1test1test1test1test1test1</li>\
+                <li>test1test1test1test1test1test2</li>\
+                <li>test1test1test1test1test1test3</li>\
+                <li>test1test1test1test1test1test4</li>\
+                <li>test1test1test1test1test1test5</li>\
+              </ul>\
+            </div>\
+            <div>\
+              <h3>화요일</h3>\
+              <ul>\
+                <li>test1test1test1test1test1test1</li>\
+                <li>test1test1test1test1test1test2</li>\
+                <li>test1test1test1test1test1test3</li>\
+                <li>test1test1test1test1test1test4</li>\
+                <li>test1test1test1test1test1test5</li>\
+              </ul>\
+            </div>\
+            <div>\
+              <h3>수요일</h3>\
+              <ul>\
+                <li>test1test1test1test1test1test1</li>\
+                <li>test1test1test1test1test1test2</li>\
+                <li>test1test1test1test1test1test3</li>\
+                <li>test1test1test1test1test1test4</li>\
+                <li>test1test1test1test1test1test5</li>\
+              </ul>\
+            </div>\
+            <div>\
+              <h3>목요일</h3>\
+              <ul>\
+                <li>test1test1test1test1test1test1</li>\
+                <li>test1test1test1test1test1test2</li>\
+                <li>test1test1test1test1test1test3</li>\
+                <li>test1test1test1test1test1test4</li>\
+                <li>test1test1test1test1test1test5</li>\
+              </ul>\
+            </div>\
+            <div>\
+              <h3>금요일</h3>\
+              <ul>\
+                <li>test1test1test1test1test1test1</li>\
+                <li>test1test1test1test1test1test2</li>\
+                <li>test1test1test1test1test1test3</li>\
+                <li>test1test1test1test1test1test4</li>\
+                <li>test1test1test1test1test1test5</li>\
+              </ul>\
+            </div>\
+          </article>\
+        </section>\
+        <section class="form_weekly">\
+          <article class="monday">\
+            <div class="time">\
+              <label for="monday">월요일</label>\
+              <input type="time" name="monday_time" placeholder="할 일">\
+            </div>\
+            <textarea name="name" rows="8" cols="80"></textarea>\
+          </article>\
+          <article class="tuesday">\
+            <div class="time">\
+              <label for="monday">화요일</label>\
+              <input type="time" name="tuesday_time" placeholder="할 일">\
+            </div>\
+            <textarea name="name" rows="8" cols="80"></textarea>\
+          </article>\
+          <article class="wednesday">\
+            <div class="time">\
+              <label for="monday">수요일</label>\
+              <input type="time" name="wednesday_time" placeholder="할 일">\
+            </div>\
+            <textarea name="name" rows="8" cols="80"></textarea>\
+          </article>\
+          <article class="thursday">\
+            <div class="time">\
+              <label for="monday">목요일</label>\
+              <input type="time" name="thursday_time" placeholder="할 일">\
+            </div>\
+            <textarea name="name" rows="8" cols="80"></textarea>\
+          </article>\
+          <article class="friday">\
+            <div class="time">\
+              <label for="monday">금요일</label>\
+              <input type="time" name="friday_time" placeholder="할 일">\
+            </div>\
+            <textarea name="name" rows="8" cols="80"></textarea>\
+          </article>\
+        </section>\
+      </div>\
+    </div>');
 
-    '.listContainer_weekly {' +
-      'width: 50%;' +
-    '}' +
-
-    '.inputContainer_weekly {' +
-      'flex: 1;' +
-      '-webkit-flex: 1;' +
-    '}' +
-
-    '.inputContainer_weekly > section > .time {' +
-      'margin: 0 0 5px 0;' +
-    '}' +
-
-    '.inputContainer_weekly > section > .time > input {' +
-
-  '}' +
-
-    '.inputContainer_weekly > section > textarea {' +
-      'width: 200px;' +
-      'margin: 0 0 10px 0;' +
-    '}'));
-    head.appendChild(style);
+  html.addClass('weekly');
 }
 
-if (!document.getElementById('container_weekly')) {
-  body.insertAdjacentHTML('beforeend', '\
-  <div id="container_weekly">\
-    <div class="listContainer_weekly">\
-      asdfasdf\
-    </div>\
-    <div class="inputContainer_weekly">\
-      <section class="monday">\
-        <div class="time">\
-          <label for="monday">월요일</label>\
-          <input type="time" name="monday_time" placeholder="할 일">\
-        </div>\
-        <textarea name="name" rows="8" cols="80"></textarea>\
-      </section>\
-      <section class="tuesday">\
-        <div class="time">\
-          <label for="monday">화요일</label>\
-          <input type="time" name="tuesday_time" placeholder="할 일">\
-        </div>\
-        <textarea name="name" rows="8" cols="80"></textarea>\
-      </section>\
-      <section class="wednesday">\
-        <div class="time">\
-          <label for="monday">수요일</label>\
-          <input type="time" name="wednesday_time" placeholder="할 일">\
-        </div>\
-        <textarea name="name" rows="8" cols="80"></textarea>\
-      </section>\
-      <section class="thursday">\
-        <div class="time">\
-          <label for="monday">목요일</label>\
-          <input type="time" name="thursday_time" placeholder="할 일">\
-        </div>\
-        <textarea name="name" rows="8" cols="80"></textarea>\
-      </section>\
-      <section class="friday">\
-        <div class="time">\
-          <label for="monday">금요일</label>\
-          <input type="time" name="friday_time" placeholder="할 일">\
-        </div>\
-        <textarea name="name" rows="8" cols="80"></textarea>\
-      </section>\
-    </div>\
-  </div>');
+if (!dimmed_weekly[0]) {
+  $.ajax({
+    url: 'FIREBASE_URL/data.json',
+    method: 'GET',
+    success: function (data) {
+      initWeekly();
+      for(var k in data) {
+        console.log(data[k]);
+      }
+    }
+  });
+} else if (dimmed_weekly.hasClass('hidden')) {
+  dimmed_weekly.removeClass('hidden');
+  html.addClass('weekly');
 } else {
-  document.getElementById('container_weekly').remove();
+  dimmed_weekly.addClass('hidden');
+  html.removeClass('weekly');
 }
