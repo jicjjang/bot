612c363ad7c9f2b6df373163e09c069b86725328
jicjjang <jicjjang12@gmail.com>
2017-07-25 19:02:38 +0900

    append modify func

--- static/js/exec.js
+++ static/js/exec.js
@@ -5,8 +5,10 @@ $(document).ready(function () {
     success: function (data) {
       var days = [];
       for (var k in data) {
+        data[k].key = k;
         days.push(data[k]);
       }
+
       days.sort(function (a, b) {
         var timeA = parseInt(a.hookTime.split(':').join(''));
         var timeB = parseInt(b.hookTime.split(':').join(''));
@@ -18,11 +20,46 @@ $(document).ready(function () {
         }
         return 0;
       });
+
       for (var k in days) {
         if (days[k].hookType === 'dooray-weeklist') {
-          $('.' + days[k].data.day + '_ul').append('<li>' + days[k].hookTime + '까지 : ' + days[k].data.text + '</li>');
+          $('.' + days[k].data.day + '_ul').append('<li data-key="' + days[k].key + '" data-time="' + days[k].hookTime + '" data-text="' + days[k].data.text + '">' + days[k].hookTime + '까지 : ' + days[k].data.text + '</li>');
         }
       }
+
+      $('.weeks_ul > li').on('click', function (e) {
+        var target = $(e.target);
+        var day = target.parent().attr('class').split('_ul weeks_ul')[0];
+        var modifyTime = $('article.' + day + ' > .time > input[type=time]');
+        var modifyText = $('article.' + day + ' > .time > input[type=text]');
+        target.addClass('choice');
+        modifyTime.val(target.data('time'));
+        modifyText.val(target.data('text'));
+
+        $('article.' + day + ' > button').off('click').on('click', function (e) {
+          $.ajax({
+            url: FIREBASE_URL + '/data/' + target.data('key') + '.json',
+            method: 'PUT',
+            data: JSON.stringify({
+              hookType: "dooray-weeklist",
+              id: bot_id,
+              description: 'weeklist',
+              image: bot_image,
+              hookTime: modifyTime.val(),
+              hookTerm: "0",
+              name: "Weeklist 봇",
+              data: {
+                text: modifyText.val(),
+                day: day
+              }
+            }),
+            success: function (data) {
+              console.log(data);
+              location.reload();
+            }
+          });
+        }).text('수정');
+      });
     }
   });
 
